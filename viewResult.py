593c5c896d9e31adcf9a64e0b73e883237d39bdc
Derviş GENÇ <76781599+dervisgenc@users.noreply.github.com>
2023-12-23 00:58:34 +0300

    Generic filter added for Results

--- viewResult.py
+++ viewResult.py
@@ -18,3 +18,18 @@ def results_page():
         table_names = repo.get_table_names()
         table_names = [name[0] for name in table_names]
         return render_template('results.html', table_names=table_names)
+    
+@viewResult.route("/results/filter_data", methods=["POST"])
+def filter_data():
+    repo = MySQLRepository()
+
+    # Handle filtering based on the submitted form data
+    filter = (request.form.get('filter_input'))
+    column = request.form.get('column_input')
+
+
+    result_data = repo.read('results',filter, column)
+    columns = repo.get_columns('results')
+    table_names = repo.get_table_names()
+    table_names = [name[0] for name in table_names]
+    return render_template('results.html', result_data=result_data, columns=columns, table_names=table_names)
