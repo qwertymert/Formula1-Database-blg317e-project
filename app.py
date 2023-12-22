33001c86661dfcb4c21f512d4b0b556d91e2ebbc
Derviş GENÇ <76781599+dervisgenc@users.noreply.github.com>
2023-12-22 04:07:04 +0300

    route rule for driver_page added

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ import yaml
 
 def create_app():
     app = Flask(__name__)
-    app.add_url_rule("/", view_func=views.home_page)   
-    # app.add_url_rule("/tables", view_func=views.table_page)
+
+    app.add_url_rule("/", view_func=views.home_page)
     app.add_url_rule("/select_table", view_func=views.select_table, methods=["GET", "POST"])
     app.add_url_rule("/filter_table", view_func=views.filter_table, methods=["GET", "POST"])
-
+    app.add_url_rule("/drivers", view_func=viewDriver.drivers_page, methods=["GET", "POST"])
     app.register_blueprint(viewDriver.viewDriver)
-    
+
     return app
 
 if __name__ == "__main__":
