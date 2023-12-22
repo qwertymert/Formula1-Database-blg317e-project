760a1de5fe0b0c9fca5763ae43d6157fc8f608f4
Derviş GENÇ <76781599+dervisgenc@users.noreply.github.com>
2023-12-23 01:26:29 +0300

    Generic filter for drivers added

--- viewDriver.py
+++ viewDriver.py
@@ -19,3 +19,18 @@ def drivers_page():
         table_names = repo.get_table_names()
         table_names = [name[0] for name in table_names]
         return render_template('drivers.html', table_names=table_names)
+
+@viewDriver.route("/drivers/filter_data", methods=["POST"])
+def filter_data():
+    repo = MySQLRepository()
+
+    # Handle filtering based on the submitted form data
+    filter = (request.form.get('filter_input'))
+    column = request.form.get('column_input')
+
+
+    drivers_data = repo.read('drivers',filter, column)
+    columns = repo.get_columns('drivers')
+    table_names = repo.get_table_names()
+    table_names = [name[0] for name in table_names]
+    return render_template('drivers.html', drivers_data=drivers_data, columns=columns, table_names=table_names)
