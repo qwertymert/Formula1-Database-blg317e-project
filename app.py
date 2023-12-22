08029fcb7e0ecabd290ae7ddad560c13f090098c
Derviş GENÇ <76781599+dervisgenc@users.noreply.github.com>
2023-12-22 20:35:18 +0300

    news paths registered

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import views
 import viewDriver
+import viewSeason
+import viewResult
 import mysql.connector
 import os
 import yaml
@@ -13,7 +15,10 @@ def create_app():
     app.add_url_rule("/select_table", view_func=views.select_table, methods=["GET", "POST"])
     app.add_url_rule("/filter_table", view_func=views.filter_table, methods=["GET", "POST"])
     app.add_url_rule("/drivers", view_func=viewDriver.drivers_page, methods=["GET", "POST"])
+    app.add_url_rule("/seasons", view_func=viewSeason.seasons_page, methods=["GET", "POST"])
+    app.add_url_rule("/results", view_func=viewResult.results_page, methods=["GET", "POST"])
     app.register_blueprint(viewDriver.viewDriver)
+    app.register_blueprint(viewSeason.viewSeason)
 
     return app
 
