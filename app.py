d690862b4ea568c36e310e9d78b004136ce8bdae
Derviş GENÇ <76781599+dervisgenc@users.noreply.github.com>
2023-12-22 22:12:25 +0300

    paths registered

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import views
 import viewDriver
 import viewSeason
 import viewResult
+import viewRace
+import viewTeam
+import viewCircuit
 import mysql.connector
 import os
 import yaml
@@ -17,8 +20,17 @@ def create_app():
     app.add_url_rule("/drivers", view_func=viewDriver.drivers_page, methods=["GET", "POST"])
     app.add_url_rule("/seasons", view_func=viewSeason.seasons_page, methods=["GET", "POST"])
     app.add_url_rule("/results", view_func=viewResult.results_page, methods=["GET", "POST"])
-    app.register_blueprint(viewDriver.viewDriver)
-    app.register_blueprint(viewSeason.viewSeason)
+    app.add_url_rule("/races",view_func=viewRace.races_page, methods=["GET", "POST"])
+    app.add_url_rule("/teams", view_func=viewTeam.teams_page, methods=["GET", "POST"])
+    app.add_url_rule("/circuits", view_func=viewCircuit.circuits_page, methods=["GET", "POST"])
+
+    app.register_blueprint(viewDriver.viewDriver, url_prefix='/')
+    app.register_blueprint(viewSeason.viewSeason, url_prefix='/')
+    app.register_blueprint(viewResult.viewResult, url_prefix='/')
+    app.register_blueprint(viewRace.viewRace, url_prefix='/')
+    app.register_blueprint(viewTeam.viewTeam, url_prefix='/')
+    app.register_blueprint(viewCircuit.viewCircuit, url_prefix='/')
+
 
     return app
 
