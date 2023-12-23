91357b36a40a157fc2aae810f68b62b45d1f3605
Derviş GENÇ <76781599+dervisgenc@users.noreply.github.com>
2023-12-23 20:17:40 +0300

    Generic filter for circuits added

--- viewCircuit.py
+++ viewCircuit.py
@@ -19,3 +19,17 @@ def circuits_page():
         table_names = repo.get_table_names()
         table_names = [name[0] for name in table_names]
         return render_template('circuits.html', table_names=table_names)
+
+@viewCircuit.route('/circuits/filter_data', methods=['POST'])
+def filter_data():
+    repo = MySqlRepository.MySQLRepository()
+
+    # Handle filtering based on the submitted form data
+    filter = (request.form.get('filter_input'))
+    column = request.form.get('column_input')
+
+    circuits_data = repo.read('circuits',filter, column)
+    columns = repo.get_columns('circuits')
+    table_names = repo.get_table_names()
+    table_names = [name[0] for name in table_names]
+    return render_template('circuits.html', circuits_data=circuits_data, columns=columns, table_names=table_names)
