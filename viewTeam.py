7b20f23eb4fc05d53120f6daf7626905123935e7
Derviş GENÇ <76781599+dervisgenc@users.noreply.github.com>
2023-12-23 20:17:52 +0300

    Generic filter for teams added

--- viewTeam.py
+++ viewTeam.py
@@ -19,3 +19,18 @@ def teams_page():
         table_names = repo.get_table_names()
         table_names = [name[0] for name in table_names]
         return render_template('teams.html', table_names=table_names)
+    
+@viewTeam.route("/teams/filter_data", methods=["POST"])
+def filter_data():
+    repo = MySqlRepository.MySQLRepository()
+
+    # Handle filtering based on the submitted form data
+    filter = (request.form.get('filter_input'))
+    column = request.form.get('column_input')
+
+
+    teams_data = repo.read('constructors',filter, column)
+    columns = repo.get_columns('constructors')
+    table_names = repo.get_table_names()
+    table_names = [name[0] for name in table_names]
+    return render_template('teams.html', teams_data=teams_data, columns=columns, table_names=table_names)
